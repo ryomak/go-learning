46f1ce678de045f1e93dc34c495d59f80e2dcec8
ryomak <ryooomaaa0413@outlook.com>
2019-06-30 20:20:54 +0900

    menu

--- image-classifier/main.go
+++ image-classifier/main.go
@@ -2,36 +2,40 @@ package main
 
 import (
 	"fmt"
+
 	brain "github.com/ryomak/deep-learning-go"
-	"github.com/ryomak/deep-learning-go/image/image_classifier"
+	iclassifier "github.com/ryomak/deep-learning-go/image/image_classifier"
 )
 
 func main() {
 	c := brain.Config{
-		ModelFile:    "model.json",
-		EpochNum:     50,
-		LearningRate: 0.01,
-		MFactor:      0.2,
-		Debug:        false,
-		HiddenNum:    50,
+		ModelFile:  "model.json",
+		EpochNum:   50,
+		Bias:       true,
+		Hiddens:    []int{1000, 100},
+		Activation: 5,
+		Mode:       0,
 	}
 	i := iclassifier.Init(
 		[]string{
-			"たんぽぽ",
-			"バラ",
-			"ラベンダー",
+			"tida",
+			"yuna",
+			"lulu",
 		},
 		"dataset",
 		30,
 		30,
 	)
 	b := brain.Init(&c, i)
-	b.LoadModel()
-	patterns, err := i.MakePattern()
+	err := b.LoadModel()
 	if err != nil {
-		panic(err)
+		patterns, err := i.MakePattern()
+		if err != nil {
+			panic(err)
+		}
+		b.NewAdamTrainer(0.01, len(patterns))
+		b.Train(patterns)
 	}
-	b.Train(patterns)
 	gazou, err := i.Decode("input.jpg")
 	if err != nil {
 		panic(err)
@@ -39,11 +43,12 @@ func main() {
 	output, err := i.Encode(b.Output(gazou))
 	fmt.Println(b.Output(gazou))
 	fmt.Println("this picture may be ", output)
+	patterns, _ := i.MakePattern()
 	sum := float64(len(patterns))
 	correct := 0.0
 	for _, p := range patterns {
-		actual, _ := i.Encode(b.Model.Update(p[0]))
-		except, _ := i.Encode(p[1])
+		actual, _ := i.Encode(b.Model.Predict(p.Input))
+		except, _ := i.Encode(p.Response)
 		if actual == except {
 			correct++
 		}
